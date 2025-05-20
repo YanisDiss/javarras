0d127f939ea229fce1c45c32c4f773a35d019c71
YanisLT <lefortyanis27@gmail.com>
2025-05-20 23:44:37 +0200

    initial commit - pls dont bully me for this

---
+++ core/src/main/java/com/yanisdiss/javarras/colors.java
@@ -0,0 +1,10 @@
+package com.yanisdiss.javarras;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class colors {
+    public static Color bg = new Color (0xaaaaaaff);
+    public static Color blue = new Color(0x00bbffff);
+    public static Color red = new Color(0xff3030ff);
+
+}
---
+++ core/src/main/java/com/yanisdiss/javarras/globals.java
@@ -0,0 +1,6 @@
+package com.yanisdiss.javarras;
+
+import com.badlogic.gdx.Gdx;
+
+public class globals {
+}
